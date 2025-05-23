e048e90f3fe1c2fcddb9231b26e1b0e301df911e
CaptEcho <70905237+CaptEcho@users.noreply.github.com>
2025-05-23 15:15:23 -0400

    DeleteContact php file created for the API

---
+++ LAMPAPI/DeleteContact.php
@@ -0,0 +1,55 @@
+<?php
+    $inData = getRequestInfo();
+
+	$conn = new mysqli("localhost", "Brayden", "password", "COP4331");	
+	if($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else{
+		try{
+			$stmt = $conn->prepare("DELETE FROM Users WHERE UserId = ?");
+			$stmt->bind_param("i", $inData["UserId"]);
+
+			if($stmt->execute()){
+				returnWithInfo();
+			}
+			else{
+				returnWithError($stmt->error);
+			}
+			$stmt->close();
+			$conn->close();
+		}
+		catch(Exception $e){
+			returnWithError($e->getMessage());
+		}	
+
+
+	}
+
+
+
+     function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = array("error" => $err , "message" => "");
+		sendResultInfoAsJson(json_encode($retValue));
+	}
+	
+	function returnWithInfo()
+	{
+		$retValue = array("error" => "" , "message" => "Contact Deleted");
+		sendResultInfoAsJson(json_encode($retValue));
+	}
+
+?>
